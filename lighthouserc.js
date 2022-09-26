81b2d861be88e55039256ad44dad1c05854b820e
Axel Goblet <axel.goblet@bigdatarepublic.nl>
2022-09-26 15:54:38 +0200

    setup urls

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,15 @@ module.exports = {
     upload: {
       target: "temporary-public-storage",
     },
+    collect: {
+      url: [
+        "http://localhost/",
+        "http://localhost/content",
+        "http://localhost/about",
+        "http://localhost/notfound",
+        "http://localhost/projects",
+      ],
+    },
     assert: {
       preset: "lighthouse:recommended",
     },
