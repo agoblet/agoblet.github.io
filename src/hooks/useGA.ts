66404b3f654fbea29e61b17414b90ad1259d5749
agoblet <40762710+agoblet@users.noreply.github.com>
2022-10-11 12:14:37 +0200

    add google analytics 4 compatibility (#11)

--- src/hooks/useGA.ts
+++ src/hooks/useGA.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 import useConsent from "./useConsent";
 
 ReactGA.initialize("G-N1RCLVWT44");
@@ -7,10 +7,10 @@ ReactGA.set({ anonymizeIp: true });
 
 export default function useGA() {
   const { state } = useConsent();
+
   useEffect(() => {
     if (state.stored === "true") {
-      console.log(window.location.pathname);
-      ReactGA.pageview(window.location.pathname);
+      ReactGA.send({ hitType: "pageview", page: window.location.pathname });
     }
   });
 }
