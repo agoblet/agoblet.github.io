dc2b9978c874be0e4dde8433e2ea0f5e8ec21938
agoblet <40762710+agoblet@users.noreply.github.com>
2022-11-23 09:41:07 +0100

    fix content filter static site generation (#22)

--- pages/content/[category].tsx
+++ pages/content/[category].tsx
@@ -1,22 +1,33 @@
 import * as React from "react";
 
 import { useRouter } from "next/router";
-import { categories } from "../../cms/categories";
-import NotFound from "../404";
+import { categories, Category } from "../../cms/categories";
 import Content from "./index";
 import { contentItems } from "../../cms/contentItems";
 
 export default function Cat() {
   const slug = useRouter().query.category as string;
-  const category = Object.values(categories).filter((c) => c.slug == slug);
-  if (category.length === 0) {
-    return <NotFound />;
-  }
+  const category = Object.values(categories).find(
+    (c) => c.slug == slug
+  ) as Category;
 
   return (
     <Content
-      items={contentItems.filter((c) => c.category === category[0])}
-      title={category[0].title}
+      items={contentItems.filter((c) => c.category === category)}
+      title={category.title}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: Object.values(categories).map((c) => {
+      return { params: { category: c.slug } };
+    }),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps() {
+  return { props: {} };
 }
