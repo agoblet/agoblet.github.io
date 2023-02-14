5256f721f7993cbd9053bce8b2c2abd0934ba081
Axel Goblet <axel.goblet@TMNL.nl>
2023-02-14 07:12:37 +0100

    add husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,9 @@ export default function App({ Component }: AppProps) {
         <main>
 
           <        PageWidth>
-            <Component>
+            <
+
+              Component>
               <CookieBanner
                 consentHook={consentHook}
                 position="relative"
